e99ceca8c4e00c69e65a87773897cd4542079ea8
Dilxe <44653007+Dilxe@users.noreply.github.com>
2020-07-31 00:53:21 +0100

    Update BypassGoogleBYC.js

--- BypassGoogleBYC.js
+++ BypassGoogleBYC.js
@@ -1,16 +1,16 @@
 // ==UserScript==
-// @name				Bypass Google's "Before You Continue"
-// @namespace		https://github.com/Dilxe/
-// @version			1
-// @description	Bypasses Google's "Before You Continue" confirmation dialog that stops the user from "using it's services" by redirecting the same search to another Google domain. (Only tested on Firefox)
-// @author			Dilxe 
+// @name        Bypass Google's "Before You Continue"
+// @namespace   https://github.com/Dilxe/
+// @version     1
+// @description Bypasses Google's "Before You Continue" confirmation dialog that stops the user from "using it's services" by automatically redirecting the same search to another Google domain. (Only tested on Firefox)
+// @author      Dilxe 
 // @include     http*://www.google.*/*
-// @exclude			http*://www.google.com/recaptcha/*
+// @exclude     http*://www.google.com/recaptcha/*
 // @include     http*://www.google.co*.*/*
 // @include     http*://news.google.*/*
 // @include     http*://encrypted.google.*/*
-// @grant				none
-// @run-at			document-end
+// @grant       none
+// @run-at      document-end
 // ==/UserScript==
 
 function Be4UContnu()
